aa2ff4302eb451ec2c2e51d5ae5c6928f2bdac24
claraortiz <clara.ortizdelatorre@gmail.com>
2023-03-15 10:22:49 +0100

    modificaciones 3-4

--- Module2-Javascript-Typescript/exercise_2/concat.js
+++ Module2-Javascript-Typescript/exercise_2/concat.js
@@ -6,12 +6,12 @@ let arr4 = [10, 11, 12];
 /*Implementa una función concat (inmutable) tal que, dados 2 arrays como entrada, 
 devuelva la concatenación de ambos. Utiliza rest / spread operators.*/
 
-const concat = (...arrs) => [ ...new Set( [].concat(...arrs) ) ];
+const concat = (a, b) => { return [...a, ...b]; };
 console.log(`Arrays concatenados: ${concat(arr1,arr2)}`);
 
 
 /*Implementa una versión del ejercicio anterior donde se acepten múltiples arrays de entrada (más de 2).*/ 
 
 
-const concat2 = (...arrs) => [ ...new Set( [].concat(...arrs) ) ];
-console.log(`Arrays concatenados: ${concat(arr1,arr2,arr3,arr4)}`);
+const concat2 = (...arrs) => { return [...arrs]; }; 
+console.log(`Arrays concatenados: ${concat2(arr1,arr2,arr3,arr4)}`);
